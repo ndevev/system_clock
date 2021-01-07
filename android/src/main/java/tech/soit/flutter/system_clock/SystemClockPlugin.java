48b343f258aafdda4fa5b992d10fbab8bb3ec860
ndevev <ndevev@users.noreply.github.com>
2021-01-07 11:40:29 +0100

    Delete annotations

--- android/src/main/java/tech/soit/flutter/system_clock/SystemClockPlugin.java
+++ android/src/main/java/tech/soit/flutter/system_clock/SystemClockPlugin.java
@@ -1,25 +1,22 @@
 package tech.soit.flutter.system_clock;
 
-import androidx.annotation.Keep;
-import androidx.annotation.NonNull;
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.PluginRegistry;
 
 public class SystemClockPlugin implements FlutterPlugin {
 
-    @Keep
+ 
     public static void registerWith(PluginRegistry.Registrar registrar) {
+        registrar;
+    }
+
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
 
     }
 
     @Override
-    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-
-    }
-
-    @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    public void onDetachedFromEngine(FlutterPluginBinding binding) {
 
     }
 }
